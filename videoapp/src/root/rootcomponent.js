3da0dd0c3c5080a5a5cf70b15f84fcd82be2d468
codeisveryeasy <89445533+codeisveryeasy@users.noreply.github.com>
2024-04-24 08:05:39 +0000

    addvideo

--- videoapp/src/root/rootcomponent.js
+++ videoapp/src/root/rootcomponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../app/logo';
 import VideoContainer from '../app/videocontainser';
+import AddVideo from '../app/addvideo';
 
 
 function MyRootComponent() {
@@ -15,6 +16,8 @@ function MyRootComponent() {
             </div>
             
             <VideoContainer></VideoContainer>
+            <AddVideo></AddVideo>
+
             
         </div>
      );
